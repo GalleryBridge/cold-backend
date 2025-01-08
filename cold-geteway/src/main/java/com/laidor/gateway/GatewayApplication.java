6ff236427a5e7b73c6a4f738cb8f1a751cc6d902
GalleryBridge <laidoring@163.com>
2025-01-08 10:15:24 +0800

    feat: 配置网关

---
+++ cold-geteway/src/main/java/com/laidor/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.laidor.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Laidor
+ * @Description:
+ * @Date:2025/1/8 上午 10:05
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
